61c5ca0d427bf5a9a95d1525db9e76b2999a32e1
Klaxer77 <goluzin-vanya@mail.ru>
2024-08-11 16:42:02 +0500

    initital

--- app/rating/models.py
+++ app/rating/models.py
@@ -7,6 +7,6 @@ class Ratings(Base):
     __tablename__ = "ratings"
     
     id = Column(Integer, primary_key=True, autoincrement=True)
-    rated_user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False, index=True)
+    rated_user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
     score = Column(Integer, nullable=False)
     
